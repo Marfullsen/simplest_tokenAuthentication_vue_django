234e1121550243178a20c75ea23aba2b25a6349a
Francisco Marfull <marfullsen@gmail.com>
2023-04-21 12:19:58 -0400

    ADD:[Models] Add an empty abstract user model.

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # add any custom user fields or methods here
+    pass
