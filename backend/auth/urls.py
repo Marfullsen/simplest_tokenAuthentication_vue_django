abfe1f563ee062d5123dc452ebf2ff74ed4e318e
Francisco Marfull <marfullsen@gmail.com>
2023-04-21 15:15:20 -0400

    ADD:[Views] Map requests to the logout view.

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import CustomLoginView
+from .views import CustomLogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
